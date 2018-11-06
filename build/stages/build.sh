9ded998b2ed00998832273c2ee9140be4b1b5ec7
Natsai, Sobitha (ESI) <sobitha.natsai@adp.com>
2018-11-06 14:59:56 +0530

    init

---
+++ build/stages/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DOCKER_REGISTRY="example.com"
+
+docker_build () {
+printf "/n Starting docker build is in-progres.../n"
+docker build --pull --label -t $DOCKER_REGISTRY:  .
+}
+
+
+docker_build
---
+++ build/stages/release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# DOCKER_REGISTRY="example.com"
+
+# docker_release () {
+# printf "/n Creating docker repo... /n"
+# printf "/n Pushing docker image to docker registery... /n"
+# docker build --pull --label -t $DOCKER_REGISTRY:  .
+# }
+
+# docker_release
+echo "release..."
